3bd706f49d9cc07117a40fd509c57c3643c466dd
samuelribeiroo <samuelribeirodev@gmail.com>
2024-10-21 21:59:59 -0300

    update(all routes): routes related to customers, stores,professionals and appointment

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,8 +1,29 @@
-import search from '@/controller/get-customer-controller'
-import register from '@/controller/register-controller'
+import createAppointment from '@/controller/appointments/create-appointment-controller'
+import authCostumer from '@/controller/customers/authenticate-customer-controller'
+import search from '@/controller/customers/get-customer-controller'
+import register from '@/controller/customers/register-controller'
+import searchProfessional from '@/controller/professionals/get-professional-controller'
+import registerProfessional from '@/controller/professionals/register-professional-controller'
+import { getAllMechanicShops } from '@/controller/stores/get-all-store-controller'
+import getStore from '@/controller/stores/get-store-controller'
+import createStore from '@/controller/stores/register-store-controller'
 import type { FastifyInstance } from 'fastify'
 
 export default async function AppRoutes(app: FastifyInstance) {
+  // Routes relationed to entity: Customers.
   app.post('/sign-up', register)
-  app.get('/search-user/:id', search) // Volta o objeto inteiro, incluindo o hash da senha e email Corrigir isso.
+  app.get('/search-user/:id', search)
+  app.post('/auth-costumer', authCostumer)
+
+  // Routes relationed to entity: Store (MechanicShop)
+  app.post('/create-store', createStore)
+  app.get('/search-store/:name', getStore)
+  app.get('/all-stores', getAllMechanicShops)
+
+  // Routes relationed to entity: Professionals.
+  app.post('/sign-up-professional', registerProfessional)
+  app.get('/professionals/:id', searchProfessional)
+
+  // Route relationed to entity: Appointment.
+  app.post('/create-appointment', createAppointment)
 }
